87992915831a4c726443bbf3201ff052239cd3b8
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-16 23:30:16 +0530

    Add files via upload
    A basic java program.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Koushik");
+        System.out.println("I am learning how to code in java");
+        System.out.println("Basically I don't know much about coding");
+        System.out.println("Though I have completed my graduation");
+    }
+}
