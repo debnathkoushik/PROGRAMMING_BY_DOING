088eaf40562362b419713b7c9b8d8bbc12f77d4d
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-17 02:59:57 +0530

    Add files via upload
    Importance of comment and slashes in a java program.

---
+++ CommentsAndSlashes.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class CommentsAndSlashes {
+
+    public static void main(String[] args)
+    {
+        System.out.println("2" + "\n" + "2");
+        //In the above line we printed two numbers in different lines
+        //Comments just makes understandibilty much easier
+    }
+}
