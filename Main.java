c1f6811b79a238df8d5dda785f19f4485a1c9869
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-16 23:25:06 +0530

    Add files via upload
    My second java program with some basic prints.

--- Main.java
+++ Main.java
@@ -6,5 +6,8 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("My name is Koushik");
+        System.out.println("I am learning how to code in java");
+        System.out.println("Basically I don't know much about coding");
+        System.out.println("Though I have completed my graduation");
     }
 }
