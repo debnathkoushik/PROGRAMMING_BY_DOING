a13a391a837ff93a0653185fd9911e33754667fe
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-17 13:32:53 +0530

    Add files via upload
    Working with variables.

---
+++ VariablesAndNames.java
@@ -0,0 +1,13 @@
+public class VariablesAndNames
+{
+	public static void main(String args[])
+	{
+		int cars, drivers, passengers;
+
+		cars = 4;
+		drivers = 3;
+		passengers = 10;
+		
+		System.out.println("We have" + cars + "cars" + "." + "\n" + "For" + passengers + "passengers" + "." + "\n" + "But only"+ drivers + "drivers are available" + "."); 
+	}
+}
