32a38da677e3d29b81aa5ab44ec9f817ad154a6b
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-19 23:44:14 +0530

    Add files via upload
    Working with more variables.

---
+++ MoreVariablesAndPrinting.java
@@ -0,0 +1,19 @@
+public class MoreVariablesAndPrinting
+{
+	public static void main(String args[])
+	{
+		String myName, myEyes, myTeeth, myHair;
+		double myAge, myHeight, myWeight;
+	
+		myName = "Koushik";
+		myEyes = "Brown";
+		myTeeth = "a bit yellow";
+		myHair = "black";
+		myAge = 24;
+		myHeight = 170.18;
+		myWeight = 80.45;
+	
+		System.out.println("My name is" + " " + myName + ".");
+		System.out.println("The colour of my eyes is" + myEyes + ".");
+	}
+}
