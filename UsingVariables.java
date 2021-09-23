056f1872fec2b5204d557ee4bf57ef03206759ed
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-23 11:32:42 +0530

    Add files via upload
    Working with some more variables.

---
+++ UsingVariables.java
@@ -0,0 +1,13 @@
+public class UsingVariables
+{
+    public static void main(String args[])
+    {
+        int ROOM_NUMBER = 110;
+        double MY_WEIGHT = 80.45;
+        String CURRENT_STATUS = "I am learning Java";
+
+        System.out.println("My room number is " +  ROOM_NUMBER);
+        System.out.println("My weight is " + MY_WEIGHT);
+        System.out.println("And " + CURRENT_STATUS);
+    }
+}
