91595e93e38fea97bff650da2ca8bbe8c4af8e55
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-23 12:50:12 +0530

    Add files via upload
    A schedule with name of subjects and their respective teachers.

---
+++ YourSchedule.java
@@ -0,0 +1,33 @@
+public class YourSchedule
+{
+    public static void main(String args[])
+    {
+        String COURSE_1 = "Object Oriented Programming";
+        String COURSE_2 = "Data Structures and Algorithms";
+        String COURSE_3 = "Database Management Systems";
+        String COURSE_4 = "Computer Networks";
+        String COURSE_5 = "Web Technology";
+        String COURSE_6 = "Systems Programming and Operating System";
+        String COURSE_7 = "Design and Analysis of Algorithms";
+        String COURSE_8 = "Software Engineering and Project Management";
+
+        String NAME_OF_TEACHER_1 = "John";
+        String NAME_OF_TEACHER_2 = "James";
+        String NAME_OF_TEACHER_3 = "Jane";
+        String NAME_OF_TEACHER_4 = "Joseph";
+        String NAME_OF_TEACHER_5 = "Thomas";
+        String NAME_OF_TEACHER_6 = "Tim";
+        String NAME_OF_TEACHER_7 = "Sara";
+        String NAME_OF_TEACHER_8 = "Sam";
+
+        System.out.println("1. " + COURSE_1 + " - " + NAME_OF_TEACHER_1);
+        System.out.println("2. " + COURSE_2 + " - " + NAME_OF_TEACHER_2);
+        System.out.println("3. " + COURSE_3 + " - " + NAME_OF_TEACHER_3);
+        System.out.println("4. " + COURSE_4 + " - " + NAME_OF_TEACHER_4);
+        System.out.println("5. " + COURSE_5 + " - " + NAME_OF_TEACHER_5);
+        System.out.println("6. " + COURSE_6 + " - " + NAME_OF_TEACHER_6);
+        System.out.println("7. " + COURSE_7 + " - " + NAME_OF_TEACHER_7);
+        System.out.println("8. " + COURSE_8 + " - " + NAME_OF_TEACHER_8);
+
+    }
+}
