ad7f1174f3d4c8d798b83e2f49f95480311a5a3c
Koushik Debnath <33828701+debnathkoushik@users.noreply.github.com>
2021-09-23 11:54:30 +0530

    Add files via upload
    A program which prints name and year of graduation.

---
+++ StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables
+{
+    public static void main(String args[])
+    {
+        String NAME = "Koushik";
+        int YEAR_OF_PASSOUT = 2021;
+
+        System.out.println("My name is " + NAME + " and I graduated in the year " + YEAR_OF_PASSOUT + ".");
+    }
+}
