7e36bfd8dd5880784c5cef658cbefb9726e05c58
Koushik Debnath <debnathkoushik605@gmail.com>
2021-09-30 18:37:23 +0530

    counts for the number of  particular characters in a string

---
+++ LetterCheck1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LetterCheck1
+{
+    public static void main(String args[])
+    {
+        Scanner InputFromUser = new Scanner(System.in);
+
+        System.out.println("Input a message:");
+        String message = InputFromUser.nextLine();
+
+        int MessageLength = message.length(); 
+
+        int StartingNumber = 0;
+        int count = 0;
+        // System.out.println("Input a ending number:");
+        // double EndingNumber = InputFromUser.nextDouble();
+
+        for ( ; StartingNumber < MessageLength ; StartingNumber++)
+        {
+            if (message.charAt(StartingNumber) == 'A' || message.charAt(StartingNumber) == 'a')
+            {
+                count++;
+            }
+        }
+
+        System.out.println("\nYour message contains the letter 'a' " + count + " times. Isn't that interesting?");
+    }
+}
