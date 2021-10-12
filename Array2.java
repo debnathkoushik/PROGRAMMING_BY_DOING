c106c405c7882cd5303a326bb41b440b0716796b
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-12 20:07:35 +0530

    printing numbers in an array using for loop

---
+++ Array2.java
@@ -0,0 +1,14 @@
+public class Array2
+{
+    public static void main(String args[])
+    {
+        int array[] = new int[10];
+        int number = -113;
+
+        for (int i = 0; i < 10; i++)
+        {
+            array[i] = -113;
+            System.out.println("At position " + i + " the number is " + array[i]);
+        }
+    }
+}
