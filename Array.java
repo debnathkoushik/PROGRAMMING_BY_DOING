d89920189ed72d3df632e061b6ecae5a2d8a3e5c
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-12 19:58:59 +0530

    a basic array

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Array
+{
+    public static void main(String args[])
+    {
+        int array[] = new int [10];
+        array[0] = -113;
+        array[1] = -113;
+        array[2] = -113;
+        array[3] = -113;
+        array[4] = -113;
+        array[5] = -113;
+        array[6] = -113;
+        array[7] = -113;
+        array[8] = -113;
+        array[9] = -113;
+
+        System.out.println("At position 1 is " + array[0]);
+        System.out.println("At position 2 is " + array[1]);
+        System.out.println("At position 3 is " + array[2]);
+        System.out.println("At position 4 is " + array[3]);
+        System.out.println("At position 5 is " + array[4]);
+        System.out.println("At position 6 is " + array[5]);
+        System.out.println("At position 7 is " + array[6]);
+        System.out.println("At position 8 is " + array[7]);
+        System.out.println("At position 9 is " + array[8]);
+        System.out.println("At position 10 is " + array[9]);
+    }
+}
