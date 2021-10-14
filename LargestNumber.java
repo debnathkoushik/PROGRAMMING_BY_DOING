e8f8e4cdfa3dd9cda69f643cd33b4dd6fb3ab16e
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-14 13:11:38 +0530

    finding the largest number in an array

---
+++ LargestNumber.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LargestNumber
+{
+    public static void main(String args[])
+    {
+        Random random_number = new Random();
+
+        int array_1[] = new int[10];
+        int count = 0;
+
+        System.out.println("The numbers are: ");
+
+        for (int i = 0; i < 10; i++)
+        {
+            array_1[i] = 1 + random_number.nextInt(100);
+
+            System.out.print(array_1[i] + "  ");
+        }
+
+        System.out.println("\n");
+
+        System.out.println("Finding the largest number!");
+        int number = array_1[0];
+        int position = 1;
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (number <array_1[i])
+            {
+               number = array_1[i];
+               position = i + 1;
+            }
+        }
+        System.out.println("The largest number is " + number + " and is present at position " + position);
+    }
+}
