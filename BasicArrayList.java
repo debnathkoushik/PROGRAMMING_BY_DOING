d2f64bba340641822cffe6f4cb15e643f174da9f
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-18 09:59:23 +0530

    storing and printing with ArrayList

---
+++ BasicArrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BasicArrayList
+{
+    public static void main(String args[])
+    {
+        ArrayList <String> list = new ArrayList <String>();
+        list.add("mangoes");
+        list.add("apples");
+        System.out.println(list);
+    }
+}
