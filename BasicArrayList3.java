9fa6fdd803ad2bbcc3bfbfe5400fc7d8eb9967b0
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-19 12:19:03 +0530

    using random in array list

---
+++ BasicArrayList3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BasicArrayList3
+{
+    public static void main(String args[])
+    {
+        ArrayList <Integer> list = new ArrayList <Integer>();
+
+        Random random_number = new Random();
+
+        int items = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            items = 1 + random_number.nextInt(100);
+            list.add(items);
+        }
+        
+        System.out.println("DIspalying all the random inputs!");
+        for (int i = 0; i < list.size(); i++)
+        {
+            System.out.println(list.get(i));
+        }
+    }
+}
