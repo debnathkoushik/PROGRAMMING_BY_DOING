6fe47a2b95a8f14f631bbaebcb0d169c6b889161
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-21 06:59:46 +0530

    sorting ana array list using exchange sort

---
+++ SortingAnArrayList1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class SortingAnArrayList1
+{
+    public static void main(String args[])
+    {
+        ArrayList <Integer> list_1 = new ArrayList <Integer>();
+
+        Scanner InputFromUser = new Scanner(System.in);
+        Random random_number = new Random();
+
+        System.out.println("Enter total number of inputs: ");
+        int number = InputFromUser.nextInt();
+
+        int items = 0;
+
+        for (int i = 0; i < number; i++)
+        {
+            items = 1 + random_number.nextInt(100);
+            list_1.add(items);
+        }
+        System.out.println("before: " + list_1);
+
+        for (int i = 0; i < list_1.size(); i++)
+        {
+            for (int j = i + 1; j < list_1.size(); j++)
+            {
+                if (list_1.get(i) > list_1.get(j))
+                {
+                    swap(list_1,  i, j);
+                }
+                System.out.println(i + "," + j + list_1);
+            }
+        }
+        
+        System.out.println("after: " + list_1);
+        
+    }
+
+    public static void swap(ArrayList <Integer> list_1, int i, int j)
+    {
+            int temp = list_1.get(i);
+            list_1.set(i, list_1.get(j));
+            list_1.set(j, temp);
+    }
+}
