3c595245e81f38c1a33e56eb6a3ea2051d08bd93
Koushik Debnath <debnathkoushik605@gmail.com>
2021-10-28 23:58:48 +0530

    usage of hasnext

---
+++ HasNext.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HasNext
+{
+    public static void main(String args[])
+    {
+        Scanner InputFromFile = new Scanner(new File("my_numbers"));
+
+    }
+}
